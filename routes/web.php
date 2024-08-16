75a1a9dbfd2ea3beb01c35060c2d2c51ba2abd1d
Gaurav <gauravss.807@gmail.com>
2024-08-16 13:13:56 +0530

    login and register page add into resource folder

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/',function()
+{
+    return view('admin.pages.login');
 });
+
+Route::get('/register',function()
+{
+    return view('admin.pages.register');
+})->name('register');
+
+Route::post('/register_save',[UserController::class,'register'])->name('register_save');
